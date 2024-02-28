7f5298a74cd8057c0b3044c70605c0d3a8482576
Egor Shchemelyov <egor2014ag@mail.ru>
2024-02-28 13:04:14 +0300

    save embeddings

--- config/model_config.py
+++ config/model_config.py
@@ -1,4 +1,13 @@
 import torch
+import json
 
 def check_cuda():
     return torch.cuda.is_available()
+
+def open_json(path_file:str):
+    
+    with open(path_file) as json_data:
+        data = json.loads(json_data)
+        json_data.close()
+        
+    return data
--- core/faiss/faiss_encoder.py
+++ core/faiss/faiss_encoder.py
@@ -1,10 +1,12 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain.embeddings import LlamaCppEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from config.model_config import check_cuda
+from NIR.config.model_config import check_cuda
+import os
+
 
 class Model_embeddings:
     
@@ -22,13 +24,38 @@ class Model_embeddings:
         else:
             self.embeddings = LlamaCppEmbeddings(model_path=model_path)
     
-    def transform(self, path_file:str):
+    def transform(self, path_file:str, academic_subject:str):
         
-        test_dict = {'pdf':PyPDFLoader(path_file)}
+        test_dict = {
+            'pdf':PyPDFLoader(path_file),
+            'txt':TextLoader(path_file),
+            'docx':Docx2txtLoader(path_file)
+            }
+        
+        file_name = path_file.split('.')
+        
         text_splitter = RecursiveCharacterTextSplitter()
-        docs = test_dict[path_file.split('.')[1]].load_and_split(text_splitter)
+        docs = test_dict[file_name[1]].load_and_split(text_splitter)
         
         faiss_docs = FAISS.from_documents(docs, self.embeddings)
+        self.save_embeddings(path_file=path_file)
         
         return faiss_docs
+    
+    def save_embeddings(self, path_file:str, faiss_docs, academic_subject:str):
+        
+        name = path_file.split('/')[-1]
+        
+        self.add_derectory(academic_subject)
+        
+        if not os.path.isdir(f"NIR/config/books/{academic_subject}/{name}"):
+            os.mkdir(f"NIR/config/books/{academic_subject}/{name}")
+        
+        faiss_docs.save_local
+        
+    def add_derectory(self, academic_subject):
+        
+        if not os.path.isdir(f"NIR/config/books/{academic_subject}"):
+            os.mkdir(f"NIR/config/books/{academic_subject}")
+            
         
