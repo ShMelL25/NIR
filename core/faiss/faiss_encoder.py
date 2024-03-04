92d92682c1fb1d6498352331dc486104cdd09e0b
Shchemelyov Egor <egor2014ag@mail.ru>
2024-03-04 10:44:05 +0300

    edit books

--- core/faiss/faiss_encoder.py
+++ core/faiss/faiss_encoder.py
@@ -38,7 +38,7 @@ class Model_embeddings:
         docs = test_dict[file_name[1]].load_and_split(text_splitter)
         
         faiss_docs = FAISS.from_documents(docs, self.embeddings)
-        self.save_embeddings(path_file=path_file, faiss_docs=faiss_docs, academic_subject=academic_subject)
+        #self.save_embeddings(path_file=path_file, faiss_docs=faiss_docs, academic_subject=academic_subject)
         
         return faiss_docs
     
